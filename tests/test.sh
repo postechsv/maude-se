99065e5d73be1f5fd0eaab5c44150187f61ada0c
smashh712 <smashh712@gmail.com>
2024-12-06 22:43:17 +0900

    set base function and vars

--- tests/test.sh
+++ tests/test.sh
@@ -1,3 +1,9 @@
+# folder containing the folder
+model_folder="models"
+
+# modes
+modes=("collapsing" "no-collapsing")
+
 # run Maude
 function run_maude {
   local filename=$1
@@ -6,3 +12,13 @@ function run_maude {
   # yices, z3, CVC4
   # timeout "$timeout_value fuck ${filename} -s yices"
 }
+
+# create a folder
+function create_folder {
+  local path=$1
+
+  if [ ! -d "$path" ]; then
+    mkdir -p "$path";
+  fi
+}
+
