89474304018967aed68a2ea97a37970e7efe24c4
geunyeol.yu <rgyenr2@gmail.com>
2025-01-17 09:54:19 +0900

    remove old directory

--- src/maude/descentSmtSignature.cc
+++
@@ -1,8 +0,0 @@
-//
-//	List of all descent functions supported by MetaLevelSmtOpSymbol.
-//	Format
-//		MACRO(symbols name, number of args)
-//
-
-// MACRO(metaSmtCheck, 3)
-MACRO(metaSmtSearch, 12)
