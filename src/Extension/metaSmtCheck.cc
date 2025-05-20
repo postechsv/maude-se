eb55ca210e6e6f42a7cb9c65fe2c0f0b8a5e7107
geunyeol.yu <rgyenr2@gmail.com>
2025-05-21 03:11:19 +0900

    update metaSmtCheck implementation

--- src/Extension/metaSmtCheck.cc
+++ src/Extension/metaSmtCheck.cc
@@ -10,6 +10,12 @@ bool MetaLevelSmtOpSymbol::metaSmtCheck(FreeDagNode *subject, RewritingContext &
 	{
 		if (Term *term = metaLevel->downTerm(subject->getArgument(1), m))
 		{
+			SymbolGetter sg;
+			sg.setModule(m);
+
+			SymbolGetter opSg;
+			opSg.setModule(getCurrentModule());
+
 			bool genAssn;
 			if (!metaLevel->downBool(subject->getArgument(3), genAssn))
 			{
@@ -20,15 +26,15 @@ bool MetaLevelSmtOpSymbol::metaSmtCheck(FreeDagNode *subject, RewritingContext &
 			DagNode *d = term->term2Dag();
 
 			const SMT_Info &smtInfo = m->getSMT_Info();
-			const char* logic = downLogic(subject->getArgument(2));
-			//   VariableGenerator vg(smtInfo);
-			//   VariableGenerator::Result result = vg.checkDag(d);
-			// VariableGenerator *vg = smtManagerFactory->create(smtInfo);
-			VariableGenerator* vg = new VariableGenerator(smtInfo, dynamic_cast<MetaLevelSmtOpSymbol*>(const_cast<MetaLevelSmtOpSymbol*>(this)));
-			vg->getConverter()->prepareFor(m);
-			vg->getConnector()->set_logic(logic);
+			const char *logic = downLogic(subject->getArgument(2));
 
-			VariableGenerator::Result result = vg->assertDag(d);
+			VariableGenerator vg(smtInfo, false);
+			vg.getConverter()->prepareFor(m);
+
+			if (logic)
+				vg.getConnector()->set_logic(logic);
+
+			VariableGenerator::Result result = vg.assertDag(d);
 			switch (result)
 			{
 			case VariableGenerator::BAD_DAG:
@@ -41,21 +47,25 @@ bool MetaLevelSmtOpSymbol::metaSmtCheck(FreeDagNode *subject, RewritingContext &
 			case VariableGenerator::SAT:
 			{
 				DagNode *r;
-				if (genAssn && result == VariableGenerator::SAT){
+				if (genAssn && result == VariableGenerator::SAT)
+				{
 					// This is Hack:
 					// we use symbol in the downed module, while we use subject's module symbol to create new expressions.
 					// We should upDagNode with different modules.
 					// MixfixModule* module = safeCast(MixfixModule*, subject->symbol()->getModule());
-					r = make_model(vg, m);
-				} else if (result == VariableGenerator::SAT_UNKNOWN){
+					r = make_model(&vg, m, &opSg);
+				}
+				else if (result == VariableGenerator::SAT_UNKNOWN)
+				{
 					r = this->unknownResultSymbol->makeDagNode();
-				} else {
+				}
+				else
+				{
 					r = metaLevel->upBool(result == VariableGenerator::SAT);
 				}
 
 				term->deepSelfDestruct();
 				(void)m->unprotect();
-				delete vg;
 
 				return context.builtInReplace(subject, r);
 			}
@@ -63,48 +73,68 @@ bool MetaLevelSmtOpSymbol::metaSmtCheck(FreeDagNode *subject, RewritingContext &
 
 			term->deepSelfDestruct();
 			(void)m->unprotect();
-			delete vg;
 		}
 	}
 	return false;
 }
 
-DagNode*  MetaLevelSmtOpSymbol::make_model(VariableGenerator* vg, MixfixModule* m){
-	SmtModel* model = vg->getModel();
-	std::vector<SmtTerm*>* keys = model->keys();
+DagNode *MetaLevelSmtOpSymbol::make_model(VariableGenerator *vg, MixfixModule *m, SymbolGetter *sg)
+{
+	SmtModel *model = vg->getModel();
+	std::vector<SmtTerm *> *keys = model->keys();
 
-	Converter* conv = vg->getConverter();
-	DagNode* result = emptySatAssnSetSymbol->makeDagNode();
-  	PointerMap qidMap;
+	ConnectedComponent *satAssnSetK = sg->getKind("SatAssignmentSet");
+	ConnectedComponent *smtCheckResK = sg->getKind("SmtCheckResult");
+	ConnectedComponent *satAssnK = sg->getKind("SatAssignment");
+	ConnectedComponent *termK = sg->getKind("Term");
+
+	Vector<ConnectedComponent *> dom;
+	Symbol *emptySatAssnSet = sg->getSymbol("empty", dom, satAssnSetK);
+
+	dom.push_back(satAssnSetK);
+	Symbol *satAssnSet = sg->getSymbol("{_}", dom, smtCheckResK);
+
+	dom.push_back(satAssnSetK);
+	Symbol *concatSatAssnSet = sg->getSymbol("_,_", dom, satAssnSetK);
+
+	dom.clear();
+	dom.push_back(termK);
+	dom.push_back(termK);
+
+	Symbol *satAssn = sg->getSymbol("_|->_", dom, satAssnK);
+
+	Converter *conv = vg->getConverter();
+
+	DagNode *result = emptySatAssnSet->makeDagNode();
+
+	PointerMap qidMap;
 	PointerMap dagMap;
-	MetaLevel* metaLevel = getMetaLevel();
+	MetaLevel *metaLevel = getMetaLevel();
 
-	for (auto k : *keys){
-		DagNode* kd = conv->term2dag(k); 
-		DagNode* kvd = conv->term2dag(model->get(k));
+	for (auto k : *keys)
+	{
+		DagNode *kd = conv->term2dag(k);
+		DagNode *kvd = conv->term2dag(model->get(k));
 
-		if (kd == nullptr || kvd == nullptr){
+		if (kd == nullptr || kvd == nullptr)
+		{
 			// cout << k << endl;
 			// cout << model->get(k) << endl;
 			continue;
 		}
 
-		Vector < DagNode * > satAssn(2);
-		satAssn[0] = metaLevel->upDagNode(kd, m, qidMap, dagMap);
-		satAssn[1] = metaLevel->upDagNode(kvd, m, qidMap, dagMap);
-		
-		Vector < DagNode * > r(2);
-		r[0] = result;
-		r[1] = satAssnSymbol->makeDagNode(satAssn);
+		Vector<DagNode *> args(2);
+		args[0] = metaLevel->upDagNode(kd, m, qidMap, dagMap);
+		args[1] = metaLevel->upDagNode(kvd, m, qidMap, dagMap);
 
-		result = concatSatAssnSetSymbol->makeDagNode(r);
+		Vector<DagNode *> r(2);
+		r[0] = result;
+		r[1] = satAssn->makeDagNode(args);
+
+		result = concatSatAssnSet->makeDagNode(r);
 	}
-	Vector < DagNode* > dag(1);
+	Vector<DagNode *> dag(1);
 	dag[0] = result;
 
-	return satAssnSetSymbol->makeDagNode(dag);
-}
-
-void printDagNodeGy(DagNode* dagNode){
-	cout << dagNode << endl;
+	return satAssnSet->makeDagNode(dag);
 }
